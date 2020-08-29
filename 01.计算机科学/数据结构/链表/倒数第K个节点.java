0381b93525f49f77250dab266c68701635f77c7c
BUGyyc <846353427@qq.com>
2020-08-29 22:15:44 +0800

    链表相交

---
+++ 01.计算机科学/数据结构/链表/倒数第K个节点.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode former = head, latter = head;
+        for(int i = 0; i < k; i++)
+            former = former.next;
+        while(former != null) {
+            former = former.next;
+            latter = latter.next;
+        }
+        return latter;
+    }
+}
