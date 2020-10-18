3cebae1403568d8bd5c3b3bd2369766cdaced4c7
BUGyyc <846353427@qq.com>
2020-10-18 21:59:51 +0800

    .

---
+++ 01.计算机科学/算法/leetcode/102.二叉树的层序遍历.cs
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=csharp
+ *
+ * [102] 二叉树的层序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (63.54%)
+ * Likes:    669
+ * Dislikes: 0
+ * Total Accepted:    206.5K
+ * Total Submissions: 325K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 
+ * 
+ * 
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回其层次遍历结果：
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
---
+++ 01.计算机科学/算法/leetcode/104.二叉树的最大深度.cs
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=csharp
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (75.17%)
+ * Likes:    720
+ * Dislikes: 0
+ * Total Accepted:    290.7K
+ * Total Submissions: 386.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最大深度。
+ * 
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回它的最大深度 3 。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+
+    }
+}
+// @lc code=end
+
