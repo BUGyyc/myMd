d0d799b4a9fb2838cbf21249d392d46dc3c893ad
BUGyyc <846353427@qq.com>
2020-10-26 23:06:02 +0800

    .

---
+++ 01.计算机科学/算法/leetcode/83.删除排序链表中的重复元素.cs
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=csharp
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (51.61%)
+ * Likes:    412
+ * Dislikes: 0
+ * Total Accepted:    150.2K
+ * Total Submissions: 291.1K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates (ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode p = head;
+        while (p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
+}
+// @lc code=end
---
+++ 01.计算机科学/算法/leetcode/85.最大矩形.cs
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=85 lang=csharp
+ *
+ * [85] 最大矩形
+ *
+ * https://leetcode-cn.com/problems/maximal-rectangle/description/
+ *
+ * algorithms
+ * Hard (48.33%)
+ * Likes:    635
+ * Dislikes: 0
+ * Total Accepted:    45K
+ * Total Submissions: 93.1K
+ * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+ *
+ * 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ * 
+ * 示例:
+ * 
+ * 输入:
+ * [
+ * ⁠ ["1","0","1","0","0"],
+ * ⁠ ["1","0","1","1","1"],
+ * ⁠ ["1","1","1","1","1"],
+ * ⁠ ["1","0","0","1","0"]
+ * ]
+ * 输出: 6
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int MaximalRectangle(char[][] matrix) {
+
+    }
+}
+// @lc code=end
+
