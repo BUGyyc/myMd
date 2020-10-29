c8ddb52cd7d3e2b23aebec03902918b41b7942d0
BUGyyc <846353427@qq.com>
2020-10-29 22:54:01 +0800

    . ...

---
+++ 01.计算机科学/算法/leetcode/114.二叉树展开为链表.cs
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=csharp
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (71.19%)
+ * Likes:    593
+ * Dislikes: 0
+ * Total Accepted:    87.7K
+ * Total Submissions: 123.3K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给定一个二叉树，原地将它展开为一个单链表。
+ * 
+ * 
+ * 
+ * 例如，给定二叉树
+ * 
+ * ⁠   1
+ * ⁠  / \
+ * ⁠ 2   5
+ * ⁠/ \   \
+ * 3   4   6
+ * 
+ * 将其展开为：
+ * 
+ * 1
+ * ⁠\
+ * ⁠ 2
+ * ⁠  \
+ * ⁠   3
+ * ⁠    \
+ * ⁠     4
+ * ⁠      \
+ * ⁠       5
+ * ⁠        \
+ * ⁠         6
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public void Flatten(TreeNode root) {
+        
+    }
+}
+// @lc code=end
+
---
+++ 01.计算机科学/算法/leetcode/137.只出现一次的数字-ii.cs
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=137 lang=csharp
+ *
+ * [137] 只出现一次的数字 II
+ *
+ * https://leetcode-cn.com/problems/single-number-ii/description/
+ *
+ * algorithms
+ * Medium (68.18%)
+ * Likes:    452
+ * Dislikes: 0
+ * Total Accepted:    44.1K
+ * Total Submissions: 64.7K
+ * Testcase Example:  '[2,2,3,2]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
+ * 
+ * 说明：
+ * 
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,2,3,2]
+ * 输出: 3
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [0,1,0,1,0,1,99]
+ * 输出: 99
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int SingleNumber(int[] nums) {
+
+    }
+}
+// @lc code=end
+
