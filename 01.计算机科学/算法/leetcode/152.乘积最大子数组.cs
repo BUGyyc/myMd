245fac30888587d47530c8e2e7ba00df62dc79f4
BUGyyc <846353427@qq.com>
2020-10-30 21:36:21 +0800

    listnode

---
+++ 01.计算机科学/算法/leetcode/152.乘积最大子数组.cs
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=152 lang=csharp
+ *
+ * [152] 乘积最大子数组
+ *
+ * https://leetcode-cn.com/problems/maximum-product-subarray/description/
+ *
+ * algorithms
+ * Medium (40.53%)
+ * Likes:    816
+ * Dislikes: 0
+ * Total Accepted:    101K
+ * Total Submissions: 249.2K
+ * Testcase Example:  '[2,3,-2,4]'
+ *
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int MaxProduct (int[] nums) {
+        int max = int.MinValue;
+        if (nums.Length == 0) return 0;
+        if (nums.Length == 1) return nums[0];
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] * max > max) {
+                max = nums[i] * max;
+            }
+        }
+    }
+}
+// @lc code=end
---
+++ 01.计算机科学/算法/leetcode/169.多数元素.cs
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=csharp
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode-cn.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (64.85%)
+ * Likes:    777
+ * Dislikes: 0
+ * Total Accepted:    228.2K
+ * Total Submissions: 351.9K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 
+ * 示例 2:
+ * 
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int MajorityElement (int[] nums) {
+        int len = nums.Length;
+        int half = len / 2;
+        Array.Sort (nums);
+        if (len <= 2) {
+            return nums[0];
+        } else if (len == 3) {
+            return (nums[0] == nums[1]) ? nums[0] : nums[2];
+        }
+        if (nums[half] == nums[half + 1]) {
+            return nums[half];
+        } else if (nums[half] == nums[half - 1]) {
+            return nums[half];
+        }
+        return nums[half];
+    }
+}
+// @lc code=end
