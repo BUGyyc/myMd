d6864385562eef104ece93fd17d290b706af91e4
delevin <846353427@qq.com>
2020-11-06 16:30:48 +0800

    回溯

--- 01.计算机科学/算法/leetcode/22.括号生成.cs
+++ 01.计算机科学/算法/leetcode/22.括号生成.cs
@@ -34,7 +34,29 @@
 // @lc code=start
 public class Solution {
     public IList<string> GenerateParenthesis(int n) {
+        List<string> list= new List<string> ();
+        BackTrack(list,0,0,n,new StringBuilder());
+        return list;
+    }
 
+    private void BackTrack(List<string> list,int open,int close,int n,StringBuilder sb){
+        if(sb.Length == 2*n){
+            list.Add(sb.ToString());
+            return;
+        }
+        if(open<n){
+            sb.Append('(');
+            BackTrack(list,open+1,close,n,sb);
+            // str.Remove(str.Length-1);
+            sb.Remove(sb.Length-1,1);
+        }
+        if(close<open){
+            sb.Append(')');
+            BackTrack(list,open,close+1,n,sb);
+            // str.Remove(str.Length-1);
+            sb.Remove(sb.Length-1,1);
+        }
+        
     }
 }
 // @lc code=end
