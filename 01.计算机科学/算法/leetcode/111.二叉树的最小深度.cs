8082f09163b3f56da4fae81aa7fe1d74c27251e3
BUGyyc <846353427@qq.com>
2020-11-08 23:41:13 +0800

    ..

---
+++ 01.计算机科学/算法/leetcode/111.二叉树的最小深度.cs
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=csharp
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (44.55%)
+ * Likes:    383
+ * Dislikes: 0
+ * Total Accepted:    143.3K
+ * Total Submissions: 321.6K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ * 
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例:
+ * 
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回它的最小深度  2.
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MinDepth (TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int min = int.MaxValue;
+        if (root.left != null) {
+            min = Math.Min (min, MinDepth (root.left));
+        }
+        if (root.right != null) {
+            min = Math.Min (min, MinDepth (root.right));
+        }
+        return min + 1;
+    }
+}
+// @lc code=end
---
+++ 01.计算机科学/算法/leetcode/118.杨辉三角.cs
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=118 lang=csharp
+ *
+ * [118] 杨辉三角
+ *
+ * https://leetcode-cn.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (67.77%)
+ * Likes:    373
+ * Dislikes: 0
+ * Total Accepted:    114.7K
+ * Total Submissions: 169.2K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ * 
+ * 
+ * 
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * 
+ * 示例:
+ * 
+ * 输入: 5
+ * 输出:
+ * [
+ * ⁠    [1],
+ * ⁠   [1,1],
+ * ⁠  [1,2,1],
+ * ⁠ [1,3,3,1],
+ * ⁠[1,4,6,4,1]
+ * ]
+ * 
+ */
+
+// @lc code=start
+using System.Collections.Generic;
+public class Solution {
+    public IList<IList<int>> Generate (int numRows) {
+        int[][] result = new int[numRows][];
+        int step = 0;
+        while (step < numRows) {
+            result[step] = new int[step + 1];
+            result[step][0] = result[step][step] = 1;
+            int j = 1;
+            while (j < step) {
+                result[step][j] = result[step - 1][j - 1] + result[step - 1][j];
+                j++;
+            }
+            step++;
+        }
+        return result;
+    }
+
+}
+// @lc code=end
