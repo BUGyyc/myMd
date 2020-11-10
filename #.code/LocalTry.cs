66427d09c4619e446edc03a8179e80a077842718
delevin <846353427@qq.com>
2020-11-10 11:48:14 +0800

    no message

--- #.code/LocalTry.cs
+++ #.code/LocalTry.cs
@@ -72,9 +72,51 @@ public class LocalTry
             }
         }
     }
+}
 
+public class Twitter {
+    Dictionary<int, List<int>> allTweetDic;
+    List<int> allFollow;
+    /** Initialize your data structure here. */
+    public Twitter () {
+        allTweetDic = new Dictionary<int, Tweet> ();
+        allFollow = new List<int> ();
+    }
 
+    /** Compose a new tweet. */
+    public void PostTweet (int userId, int tweetId) {
+        if (allTweetDic.ContainsKey (userId)) {
+            var list = allTweetDic[userId];
+            list.Insert (0,tweetId);
+            allTweetDic[userId] = list;
+        } else {
+            List<int> list = new List<int> ();
+            list.Insert (0,tweetId);
+            allTweetDic.Add (userId, list);
+        }
+    }
 
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    public IList<int> GetNewsFeed (int userId) {
+        if (allFollow == null || allFollow.Count < 1 || allFollow.Contains (userId) == false) {
+            return new List<int> ();
+        } else 
+        if(allTweetDic.ContainsKey(userId)){
+            return allTweetDic[userId];
+        }else{
+            return new List<int>();
+        }
+    }
+
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    public void Follow (int followerId, int followeeId) {
+
+    }
+
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    public void Unfollow (int followerId, int followeeId) {
+
+    }
 }
 
 public struct TreeNode
---
+++ 01.计算机科学/算法/leetcode/862.和至少为-k-的最短子数组.cs
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=862 lang=csharp
+ *
+ * [862] 和至少为 K 的最短子数组
+ *
+ * https://leetcode-cn.com/problems/shortest-subarray-with-sum-at-least-k/description/
+ *
+ * algorithms
+ * Hard (16.25%)
+ * Likes:    225
+ * Dislikes: 0
+ * Total Accepted:    10.2K
+ * Total Submissions: 62.5K
+ * Testcase Example:  '[1]\n1'
+ *
+ * 返回 A 的最短的非空连续子数组的长度，该子数组的和至少为 K 。
+ * 
+ * 如果没有和至少为 K 的非空子数组，返回 -1 。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：A = [1], K = 1
+ * 输出：1
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：A = [1,2], K = 4
+ * 输出：-1
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：A = [2,-1,2], K = 3
+ * 输出：3
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= A.length <= 50000
+ * -10 ^ 5 <= A[i] <= 10 ^ 5
+ * 1 <= K <= 10 ^ 9
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int ShortestSubarray(int[] A, int K) {
+
+    }
+}
+// @lc code=end
+
