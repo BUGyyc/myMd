980786712a2dbdc34480234f8abc1eda1b79ca4b
BUGyyc <846353427@qq.com>
2020-12-20 12:14:35 +0800

    leetcode 200 注解

---
+++ 01.计算机科学/算法/leetcode/664.奇怪的打印机.cs
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=664 lang=csharp
+ *
+ * [664] 奇怪的打印机
+ *
+ * https://leetcode-cn.com/problems/strange-printer/description/
+ *
+ * algorithms
+ * Hard (47.78%)
+ * Likes:    93
+ * Dislikes: 0
+ * Total Accepted:    3.4K
+ * Total Submissions: 7.1K
+ * Testcase Example:  '"aaabbb"'
+ *
+ * 有台奇怪的打印机有以下两个特殊要求：
+ * 
+ * 
+ * 打印机每次只能打印同一个字符序列。
+ * 每次可以在任意起始和结束位置打印新字符，并且会覆盖掉原来已有的字符。
+ * 
+ * 
+ * 给定一个只包含小写英文字母的字符串，你的任务是计算这个打印机打印它需要的最少次数。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: "aaabbb"
+ * 输出: 2
+ * 解释: 首先打印 "aaa" 然后打印 "bbb"。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: "aba"
+ * 输出: 2
+ * 解释: 首先打印 "aaa" 然后在第二个位置打印 "b" 覆盖掉原来的字符 'a'。
+ * 
+ * 提示: 输入字符串的长度不会超过 100。
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    //TODO:
+    public int StrangePrinter(string s) {
+
+    }
+}
+// @lc code=end
+
