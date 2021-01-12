1cba7075b72c44d7dd180dcbd4020a10ffc50bf5
delevin <846353427@qq.com>
2021-01-12 11:33:31 +0800

    .

---
+++ #.code/Lc5.cs
@@ -0,0 +1,17 @@
+/*
+ * @Author: delevin.ying 
+ * @Date: 2021-01-12 11:16:21 
+ * @Last Modified by: delevin.ying
+ * @Last Modified time: 2021-01-12 11:18:08
+ */
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+public class Lc5
+{
+    public IList<string> GetAllClock(int n)
+    {
+        if(n == 0)
+    }
+}
