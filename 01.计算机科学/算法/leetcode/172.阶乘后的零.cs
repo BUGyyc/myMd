cbab69b122fca963e42f15bb50da8dc2d3033822
BUGyyc <846353427@qq.com>
2021-01-24 20:51:39 +0800

    .

---
+++ 01.计算机科学/算法/leetcode/172.阶乘后的零.cs
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=172 lang=csharp
+ *
+ * [172] 阶乘后的零
+ *
+ * https://leetcode-cn.com/problems/factorial-trailing-zeroes/description/
+ *
+ * algorithms
+ * Easy (40.48%)
+ * Likes:    376
+ * Dislikes: 0
+ * Total Accepted:    52.6K
+ * Total Submissions: 130K
+ * Testcase Example:  '3'
+ *
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 3
+ * 输出: 0
+ * 解释: 3! = 6, 尾数中没有零。
+ * 
+ * 示例 2:
+ * 
+ * 输入: 5
+ * 输出: 1
+ * 解释: 5! = 120, 尾数中有 1 个零.
+ * 
+ * 说明: 你算法的时间复杂度应为 O(log n) 。
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public int TrailingZeroes(int n) {
+
+    }
+}
+// @lc code=end
+
---
+++ 01.计算机科学/算法/leetcode/541.反转字符串-ii.cs
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=csharp
+ *
+ * [541] 反转字符串 II
+ *
+ * https://leetcode-cn.com/problems/reverse-string-ii/description/
+ *
+ * algorithms
+ * Easy (56.74%)
+ * Likes:    112
+ * Dislikes: 0
+ * Total Accepted:    27.5K
+ * Total Submissions: 48.5K
+ * Testcase Example:  '"abcdefg"\n2'
+ *
+ * 给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+ * 
+ * 
+ * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+ * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ * 
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "bacdfeg"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 该字符串只包含小写英文字母。
+ * 给定字符串的长度和 k 在 [1, 10000] 范围内。
+ * 
+ * 
+ */
+
+// @lc code=start
+public class Solution {
+    public string ReverseStr(string s, int k) {
+
+    }
+}
+// @lc code=end
+
